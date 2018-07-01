1a7580375e5ce0699edfa4cdd367a30b7752c48a
Paolo <dbforum05@email.it>
2018-07-01 09:58:01 +0200

    Update main.cpp
    Manage some input on argv...

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
 #include <ctime>
+#include "stdafx.h"
 #include "_010EditorKeygen.hpp"
+#include <tchar.h>
+#include <windows.h>
 
-int main() {
-    std::string name;
-    std::cout << "Input your name(English Only): ";
-    std::getline(std::cin, name);
+int main(int argc, CHAR* argv[])
+{
+	if (argc < 5) {
+		_tprintf_s(TEXT("Usage:\n"));
+		_tprintf_s(TEXT("        010Editor-keygen.exe <your name> <year> <month> <day> <numbers of user>\n\n"));
+		_tprintf_s(TEXT("Example:\n\n"));
+		_tprintf_s(TEXT("        010Editor-keygen.exe DeltaFoX 2106 02 08 999\n"));
+		return 0;
+	}
 
-    std::tm ZeroDate = { 0, 0, 0, 1, 1, 1970 - 1900 };
+	std::string name(argv[1]);
 
-    int ExpireYear = 2020;
-    int ExpireMonth = 6;
-    int ExpireDay = 23;
-    std::tm ExpireDate = { 0, 0, 0, ExpireDay, ExpireMonth, ExpireYear - 1900 };
-    uint32_t ExpireDaystamp = static_cast<uint32_t>(std::difftime(std::mktime(&ExpireDate), std::mktime(&ZeroDate)) / 3600 / 24);
+	std::tm ZeroDate = { 0, 0, 0, 1, 1, 1970 - 1900 };
 
-    std::vector<byte> name_bytes(name.begin(), name.end());
-    _010Editor::Keygen<_010Editor::KeyType::TimeLicense> keygen;
-    std::cout << keygen.GetKey(name_bytes, ExpireDaystamp, 1) << std::endl;
-    return 0;
+	int ExpireYear = atoi(argv[2]);
+	int ExpireMonth = atoi(argv[3]);
+	int ExpireDay = atoi(argv[4]);
+	int LicenseCount = (argv[5] != NULL && atoi(argv[5]) > 0 && atoi(argv[5]) < 1000) ? atoi(argv[5]) : 1;
+	std::tm ExpireDate = { 0, 0, 0, ExpireDay + 2, ExpireMonth, ExpireYear - 1900 };
+	uint32_t ExpireDaystamp = static_cast<uint32_t>(std::difftime(std::mktime(&ExpireDate), std::mktime(&ZeroDate)) / 3600 / 24);
+
+	std::vector<byte> name_bytes(name.begin(), name.end());
+	_010Editor::Keygen<_010Editor::KeyType::TimeLicense> keygen;
+	std::cout << keygen.GetKey(name_bytes, ExpireDaystamp, LicenseCount) << std::endl;
+	return 1;
 }
